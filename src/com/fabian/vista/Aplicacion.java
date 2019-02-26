3511f4b11ab5dcbd340c3a3490f2b99b123a2e19
fabian <fabian@Chicaiza>
2019-02-26 08:40:01 -0500

    Trabajo_final

---
+++ src/com/fabian/vista/Aplicacion.java
@@ -0,0 +1,15 @@
+package com.fabian.vista;
+
+
+import com.fabian.modelo.VentanaPuntos;
+
+/**
+ *
+ * @author Casa
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        new VentanaPuntos();
+    }
+    
+}
